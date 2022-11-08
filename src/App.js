02b3591938732ca8920f9aeea99bf3c5081a6899
KARUNAKARPUSULURI <97221366+KARUNAKARPUSULURI@users.noreply.github.com>
2022-11-08 12:34:36 +0530

    timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from './components/Login';
-
-function App() {
-  return (
-    <>
-        <Login />
-    </>
-  );
+import React, { useState, useRef, useEffect } from 'react'
+import StopWatch from './components/StopWatch';
+const App = () => {
+const Ref = useRef(null);
+const [timer, setTimer] = useState('00:00:00');
+const getTimeRemaining = (e) => {
+const total = Date.parse(e) - Date.parse(new Date());
+const seconds = Math.floor((total / 1000) % 60);
+const minutes = Math.floor((total / 1000 / 60) % 60);
+const hours = Math.floor((total / 1000 * 60 * 60) % 24);
+return {
+total, hours, minutes, seconds
+};
 }
-
-export default App;
+const startTimer = (e) => {
+let { total, hours, minutes, seconds }
+= getTimeRemaining(e);
+if (total >= 0) {
+setTimer(
+(hours > 9 ? hours : '0' + hours) + ':' +
+(minutes > 9 ? minutes : '0' + minutes) + ':'
++ (seconds > 9 ? seconds : '0' + seconds)
+)
+}
+}
+const clearTimer = (e) => {
+if (Ref.current) clearInterval(Ref.current);
+const id = setInterval(() => {
+startTimer(e);
+}, 1000)
+Ref.current = id;
+}
+const getDeadTime = () => {
+let deadline = new Date();
+deadline.setSeconds(deadline.getSeconds() + 10);
+return deadline;
+}
+useEffect(() => {
+clearTimer(getDeadTime());
+}, []);
+const onClickReset = () => {
+clearTimer(getDeadTime());
+}
+return (
+<div className="Codedamn App">
+  <StopWatch />
+<h2>{timer}</h2>
+<button onClick={onClickReset}>Reset</button>
+</div>
+)
+}
+export default App
